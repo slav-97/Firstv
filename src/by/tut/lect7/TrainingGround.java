14c00239384fb27d8157da6bacc83115b6572995
slav__lebedko_mail_ru <slav97.lebedko@mail.ru>
2020-03-06 09:23:10 +0300

    Tasks

--- src/by/tut/lect7/TrainingGround.java
+++ src/by/tut/lect7/TrainingGround.java
@@ -3,8 +3,8 @@ package by.tut.lect7;
 public class TrainingGround {
     public static void main(String[] args) {
         Enemy enemy = new Enemy(1000);
-        Wizard wizard = new Wizard("Wizard", 30);
-        wizard.attackEnemy(enemy);
+        Mage mage = new Mage("Mage", 30);
+        mage.attackEnemy(enemy);
         Archer archer = new Archer("Archer", 30);
         archer.attackEnemy(enemy);
         Warrior warrior = new Warrior("Warrior", 40);
