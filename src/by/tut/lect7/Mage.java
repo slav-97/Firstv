45b43991b018160fb883b72fe57e89aecbc57c6a
slav__lebedko_mail_ru <slav97.lebedko@mail.ru>
2020-03-06 09:15:08 +0300

    Tasks

---
+++ src/by/tut/lect7/Mage.java
@@ -0,0 +1,7 @@
+package by.tut.lect7;
+
+public class Mage extends BaseHero{
+    public Mage(String name, int damage) {
+        super(name, damage);
+    }
+}
--- src/by/tut/lect7/Wizard.java
+++
@@ -1,7 +0,0 @@
-package by.tut.lect7;
-
-public class Wizard extends BaseHero{
-    public Wizard(String name, int damage) {
-        super(name, damage);
-    }
-}
